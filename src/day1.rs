743b98a6bd509941906874f2e18c6bfffc55342c
Ben Jacobson <bentjacobson@gmail.com>
2025-12-01 23:46:50 -0800

    Solve day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,17 +1,78 @@
 use crate::tools::read_file;
 
-type InputData = i32;
+use std::iter;
 
-fn parse_input(input: &str) -> InputData {
-    0
+#[derive(Debug, Clone, Copy)]
+enum Dir {
+    Left,
+    Right,
 }
 
-fn part1(input_data: &InputData) {}
+#[derive(Debug, Clone, Copy)]
+struct Rotation {
+    dir: Dir,
+    ticks: i32,
+}
 
-fn part2(input_data: &InputData) {}
+const MOD: i32 = 100;
+
+type InputData = Vec<Rotation>;
+
+fn parse_input(input: &str) -> InputData {
+    input
+        .trim()
+        .lines()
+        .map(|line| Rotation {
+            dir: match &line[0..1] {
+                "L" => Dir::Left,
+                _ => Dir::Right,
+            },
+            ticks: line[1..line.len()].parse().unwrap(),
+        })
+        .collect()
+}
+
+fn part1(input_data: &InputData) {
+    let mut zero_count = 0;
+    let mut val: i32 = 50;
+    for Rotation { dir, ticks } in input_data.iter() {
+        val += match dir {
+            Dir::Left => -*ticks + MOD,
+            Dir::Right => *ticks,
+        };
+        val %= MOD;
+
+        if val == 0 {
+            zero_count += 1;
+        }
+    }
+
+    println!("day1 part1: {}", zero_count);
+}
+
+fn part2(input_data: &InputData) {
+    let mut zero_count = 0;
+    let mut val = 50;
+    let sign = |rot| match rot {
+        Dir::Left => -1,
+        Dir::Right => 1,
+    };
+    let moves = input_data
+        .iter()
+        .map(|rot| iter::repeat(sign(rot.dir)).take(rot.ticks as usize))
+        .flatten();
+    for m in moves {
+        val += m;
+        if val % MOD == 0 {
+            zero_count += 1;
+        }
+    }
+
+    println!("day1 part2: {}", zero_count);
+}
 
 pub fn solve() {
-    let input_data = parse_input(&read_file("inputs/day1_example.txt"));
+    let input_data = parse_input(&read_file("inputs/day1.txt"));
     part1(&input_data);
     part2(&input_data);
 }
