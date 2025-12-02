fa2ce2d61c674fde0af29f27f47f169f245115a6
Ben Jacobson <bentjacobson@gmail.com>
2025-12-01 23:45:12 -0800

    Outline daily problem structure

---
+++ src/day5.rs
@@ -0,0 +1,17 @@
+use crate::tools::read_file;
+
+type InputData = i32;
+
+fn parse_input(input: &str) -> InputData {
+    0
+}
+
+fn part1(input_data: &InputData) {}
+
+fn part2(input_data: &InputData) {}
+
+pub fn solve() {
+    let input_data = parse_input(&read_file("inputs/day5_example.txt"));
+    part1(&input_data);
+    part2(&input_data);
+}
