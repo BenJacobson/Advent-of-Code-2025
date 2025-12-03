b15b0579e5c716b653fd07ca75aaee0f277cbcdc
Ben Jacobson <bentjacobson@gmail.com>
2025-12-02 22:57:47 -0800

    Solve day 3

--- src/day3.rs
+++ src/day3.rs
@@ -1,17 +1,50 @@
 use crate::tools::read_file;
 
-type InputData = i32;
+type InputData = Vec<String>;
 
 fn parse_input(input: &str) -> InputData {
-    0
+    input.trim().lines().map(|s| s.to_string()).collect()
 }
 
-fn part1(input_data: &InputData) {}
+fn get_joltage(s: &str, n: usize) -> u64 {
+    if s.len() < n {
+        return 0;
+    }
 
-fn part2(input_data: &InputData) {}
+    let digits = "9876543210";
+    let mut start_index = 0;
+    let mut joltage = Vec::new();
+    for j in 0..n {
+        let digit_index = start_index
+            + digits
+                .chars()
+                .flat_map(|d| s[start_index..(s.len() - n + j + 1)].find(d))
+                .next()
+                .unwrap();
+        joltage.push(digit_index);
+        start_index = digit_index + 1;
+    }
+
+    let mut ans: u64 = 0;
+    for index in joltage {
+        ans *= 10;
+        ans += s.chars().nth(index).unwrap().to_digit(10).unwrap() as u64;
+    }
+    ans
+}
+
+fn part1(input_data: &InputData) {
+    let total_joltage: u64 = input_data.iter().map(|s| get_joltage(s, 2)).sum();
+    println!("day3 part1: {}", total_joltage);
+}
+
+fn part2(input_data: &InputData) {
+    let total_joltage: u64 = input_data.iter().map(|s| get_joltage(s, 12)).sum();
+    println!("day3 part2: {}", total_joltage);
+}
 
 pub fn solve() {
-    let input_data = parse_input(&read_file("inputs/day3_example.txt"));
+    let input_data = parse_input(&read_file("inputs/day3.txt"));
     part1(&input_data);
     part2(&input_data);
 }
