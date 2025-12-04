459fb03a69a0b4b90f567ddc840fcbae59555f64
Ben Jacobson <bentjacobson@gmail.com>
2025-12-03 21:06:46 -0800

    Day 2 performance improvements

--- src/day2.rs
+++ src/day2.rs
@@ -1,8 +1,9 @@
 use crate::tools::read_file;
 
+use rayon::prelude::*;
 use std::iter;
 
-type InputData = Vec<(i64, i64)>;
+type InputData = Vec<(u64, u64)>;
 
 fn parse_input(input: &str) -> InputData {
     input
@@ -15,23 +16,36 @@ fn parse_input(input: &str) -> InputData {
         .collect()
 }
 
-fn has_double_sequence(n: &i64) -> bool {
-    let s = n.to_string();
-    let half = s.len() / 2;
-    return s[0..half] == s[half..s.len()];
+fn extract_digits(mut n: u64, digit_buffer: &mut Vec<u8>) {
+    digit_buffer.clear();
+
+    while n > 0 {
+        digit_buffer.push((n % 10) as u8);
+        n /= 10;
+    }
 }
 
-fn has_repeated_sequence(n: &i64) -> bool {
-    let s = n.to_string();
-    for i in 1..=(s.len() / 2) {
-        if s.len() % i != 0 {
+fn has_double_sequence(n: u64, digit_buffer: &mut Vec<u8>) -> bool {
+    extract_digits(n, digit_buffer);
+    let half = digit_buffer.len() / 2;
+    return digit_buffer[0..half] == digit_buffer[half..digit_buffer.len()];
+}
+
+fn has_repeated_sequence(n: u64, digit_buffer: &mut Vec<u8>) -> bool {
+    extract_digits(n, digit_buffer);
+    for i in 1..=(digit_buffer.len() / 2) {
+        if digit_buffer.len() % i != 0 {
             continue;
         }
-        let s_repeat: String = iter::repeat(s[0..i].chars())
-            .flatten()
-            .take(s.len())
-            .collect();
-        if s == s_repeat {
+        let mut all_match = true;
+        for j in 1..(digit_buffer.len() / i) {
+            let start = i * j;
+            if digit_buffer[0..i] != digit_buffer[start..start + i] {
+                all_match = false;
+                break;
+            }
+        }
+        if all_match {
             return true;
         }
     }
@@ -39,19 +53,27 @@ fn has_repeated_sequence(n: &i64) -> bool {
 }
 
 fn part1(input_data: &InputData) {
-    let count: i64 = input_data
-        .iter()
-        .flat_map(|(start, end)| *start..=*end)
-        .filter(has_double_sequence)
+    let count: u64 = input_data
+        .par_iter()
+        .map(|(start, end)| {
+            let mut digit_buffer = Vec::new();
+            (*start..=*end)
+                .filter(|n| has_double_sequence(*n, &mut digit_buffer))
+                .sum::<u64>()
+        })
         .sum();
     println!("day2 part1: {}", count);
 }
 
 fn part2(input_data: &InputData) {
-    let count: i64 = input_data
-        .iter()
-        .flat_map(|(start, end)| *start..=*end)
-        .filter(has_repeated_sequence)
+    let count: u64 = input_data
+        .par_iter()
+        .map(|(start, end)| {
+            let mut digit_buffer = Vec::new();
+            (*start..=*end)
+                .filter(|n| has_repeated_sequence(*n, &mut digit_buffer))
+                .sum::<u64>()
+        })
         .sum();
     println!("day2 part2: {}", count);
 }
