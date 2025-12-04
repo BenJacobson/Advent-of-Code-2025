47b6d96e5eca82ca883e1d4b3508f9bb8fb44065
Ben Jacobson <bentjacobson@gmail.com>
2025-12-03 22:05:05 -0800

    Day 4 performance improvements

--- src/day4.rs
+++ src/day4.rs
@@ -2,7 +2,7 @@ use crate::tools::read_file;
 
 use std::collections::HashSet;
 
-type InputData = HashSet<(i32, i32)>;
+type InputData = Vec<Vec<bool>>;
 
 const DIRS: [(i32, i32); 8] = [
     (1, 1),
@@ -16,28 +16,29 @@ const DIRS: [(i32, i32); 8] = [
 ];
 
 fn parse_input(input: &str) -> InputData {
-    let grid: Vec<&str> = input.lines().collect();
-    let mut rolls = HashSet::new();
+    input
+        .lines()
+        .map(|line| line.chars().map(|c| c == '@').collect())
+        .collect()
+}
 
-    for i in 0..grid.len() {
-        let line = grid[i];
-        for (j, c) in line.chars().enumerate() {
-            if c != '@' {
-                continue;
-            }
-            rolls.insert((i as i32, j as i32));
-        }
-    }
-
-    rolls
+fn get_data(grid: &InputData, i: i32, j: i32) -> bool {
+    0 <= i
+        && i < grid.len() as i32
+        && 0 <= j
+        && j < grid[i as usize].len() as i32
+        && grid[i as usize][j as usize]
 }
 
 fn part1(input_data: &InputData) {
     let count = input_data
         .iter()
-        .filter(|(i, j)| {
-            DIRS.iter()
-                .filter(|(di, dj)| input_data.contains(&(i + di, j + dj)))
+        .enumerate()
+        .flat_map(|(i, line)| line.iter().enumerate().map(move |(j, v)| (i, j, *v)))
+        .filter(|(i, j, v)| {
+            *v && DIRS
+                .iter()
+                .filter(|(di, dj)| get_data(input_data, (*i as i32) + di, (*j as i32) + dj))
                 .count()
                 < 4
         })
@@ -46,24 +47,32 @@ fn part1(input_data: &InputData) {
 }
 
 fn part2(input_data: &InputData) {
-    let mut rolls: InputData = input_data.clone();
+    let mut grid: InputData = input_data.clone();
     loop {
-        let mut new_rolls: InputData = rolls
-            .iter()
-            .filter(|(i, j)| {
-                DIRS.iter()
-                    .filter(|(di, dj)| rolls.contains(&(i + di, j + dj)))
+        let mut modified = false;
+        for i in 0..grid.len() {
+            let line = &grid[i];
+            for j in 0..line.len() {
+                if !grid[i][j] {
+                    continue;
+                }
+                let clear = DIRS
+                    .iter()
+                    .filter(|(di, dj)| get_data(&grid, (i as i32) + di, (j as i32) + dj))
                     .count()
-                    >= 4
-            })
-            .map(|roll| *roll)
-            .collect();
-        if rolls.len() == new_rolls.len() {
+                    < 4;
+                if clear {
+                    grid[i][j] = false;
+                    modified = true;
+                }
+            }
+        }
+        if !modified {
             break;
         }
-        rolls = new_rolls;
     }
-    let removed = input_data.len() - rolls.len();
+    let removed = input_data.iter().flatten().filter(|v| **v).count()
+        - grid.iter().flatten().filter(|v| **v).count();
     println!("day4 part2: {}", removed);
 }
 
