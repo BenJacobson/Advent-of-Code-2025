32eb5f19065542cbbbc624ceed282cd9cab99604
Ben Jacobson <bentjacobson@gmail.com>
2025-12-06 22:27:04 -0800

    Solve day 6

--- src/day6.rs
+++ src/day6.rs
@@ -1,14 +1,96 @@
+use itertools::Itertools;
+
 use crate::tools::read_file;
 
-type InputData = i32;
+use std::iter::zip;
+use std::os::raw;
+use std::str::Chars;
 
-fn parse_input(input: &str) -> InputData {
-    0
+type Equation = (Vec<Vec<char>>, fn(u64, u64) -> u64);
+type InputData = Vec<Equation>;
+
+fn parse_equation(mut equation_strs: Vec<Vec<char>>) -> Equation {
+    let last = equation_strs.pop().unwrap();
+    let op = match last.into_iter().find_or_first(|c| !char::is_whitespace(*c)) {
+        Some('*') => |a: u64, b: u64| a * b,
+        _ => |a: u64, b: u64| a + b,
+    };
+    (equation_strs, op)
 }
 
-fn part1(input_data: &InputData) {}
+fn parse_input(input: &str) -> InputData {
+    let mut lines: Vec<Chars> = input.lines().map(|line| line.chars()).collect();
+    let n = lines.len();
+    let new_raw_equation = || {
+        let mut v = Vec::new();
+        for _ in 0..n {
+            v.push(Vec::new());
+        }
+        v
+    };
 
-fn part2(input_data: &InputData) {}
+    let mut raw_equations: Vec<Vec<Vec<char>>> = Vec::new();
+    raw_equations.push(new_raw_equation());
+    'outer: loop {
+        let mut char_vec = Vec::new();
+        for i in 0..n {
+            let Some(char) = lines[i].next() else {
+                break 'outer;
+            };
+            char_vec.push(char);
+        }
+        if char_vec.iter().all(|c| char::is_whitespace(*c)) {
+            raw_equations.push(new_raw_equation());
+        } else {
+            let last_index = raw_equations.len() - 1;
+            for i in 0..n {
+                raw_equations[last_index][i].push(char_vec[i]);
+            }
+        }
+    }
+
+    raw_equations.into_iter().map(parse_equation).collect()
+}
+
+fn compute_equation((raw_nums, op): &Equation) -> u64 {
+    raw_nums
+        .iter()
+        .map(|v| {
+            v.iter()
+                .filter(|c| !c.is_whitespace())
+                .collect::<String>()
+                .parse()
+                .unwrap()
+        })
+        .reduce(op)
+        .unwrap()
+}
+
+fn rotate_90(data: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut result = Vec::new();
+    let n = data[0].len();
+    for j in 0..n {
+        let mut v: Vec<char> = Vec::new();
+        for i in 0..data.len() {
+            v.push(data[i][j]);
+        }
+        result.push(v);
+    }
+    result
+}
+
+fn part1(input_data: &InputData) {
+    let total = input_data.iter().map(compute_equation).sum::<u64>();
+    println!("day6 part1: {}", total);
+}
+
+fn part2(input_data: &InputData) {
+    let total = input_data
+        .iter()
+        .map(|(raw_nums, op)| compute_equation(&(rotate_90(raw_nums), *op)))
+        .sum::<u64>();
+    println!("day6 part2: {}", total);
+}
 
 pub fn solve() {
     let input_data = parse_input(&read_file("inputs/day6_example.txt"));
