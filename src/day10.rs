e0a1c4ab7d2255ef37778c9330ca24783efa1701
Ben Jacobson <bentjacobson@gmail.com>
2025-12-10 23:08:11 -0800

    Day 10 performance improvements

--- src/day10.rs
+++ src/day10.rs
@@ -1,11 +1,11 @@
 use crate::tools::read_file;
 
 use rayon::prelude::*;
-use z3::Solver;
 use z3::ast::Int;
+use z3::{Optimize, SatResult};
 
 struct Machine {
-    indicators: Vec<bool>,
+    indicators: u16,
     toggles: Vec<Vec<u16>>,
     joltage: Vec<u16>,
 }
@@ -22,10 +22,12 @@ fn parse_num_set(num_set: &str) -> Vec<u16> {
 fn parse_line(line: &str) -> Machine {
     let words: Vec<&str> = line.split_whitespace().collect();
 
-    let indicators = words[0][1..words[0].len() - 1]
+    let indicators_str = words[0][1..words[0].len() - 1]
         .chars()
-        .map(|c| c == '#')
-        .collect();
+        .rev()
+        .map(|c| if c == '#' { '1' } else { '0' })
+        .collect::<String>();
+    let indicators = u16::from_str_radix(indicators_str.as_str(), 2).unwrap();
     let toggles = words[1..words.len() - 1]
         .iter()
         .map(|num_set| parse_num_set(*num_set))
@@ -51,11 +53,11 @@ fn solve_machine_indicators(machine: &Machine) -> u32 {
             continue;
         }
 
-        let mut on = vec![false; machine.indicators.len()];
+        let mut on: u16 = 0;
         for i in 0..machine.toggles.len() {
             if config & (1 << i) != 0 {
                 for j in machine.toggles[i].iter() {
-                    on[*j as usize] = !on[*j as usize];
+                    on ^= 1 << *j;
                 }
             }
         }
@@ -79,13 +81,13 @@ fn solve_machine_joltage(machine: &Machine) -> u32 {
     let toggle_vars: Vec<Int> = (0..machine.toggles.len())
         .map(|i| Int::fresh_const(i.to_string().as_str()))
         .collect();
-    let solver = Solver::new();
+    let solver = Optimize::new();
     for toggle_var in toggle_vars.iter() {
-        solver.assert(toggle_var.ge(0));
+        solver.assert(&toggle_var.ge(0));
     }
     for ji in 0..machine.joltage.len() {
         solver.assert(
-            (0..machine.toggles.len())
+            &(0..machine.toggles.len())
                 .filter(|ti| machine.toggles[*ti].contains(&(ji as u16)))
                 .map(|ti| toggle_vars[ti].clone())
                 .reduce(|acc, int| acc + int)
@@ -93,12 +95,20 @@ fn solve_machine_joltage(machine: &Machine) -> u32 {
                 .eq(machine.joltage[ji]),
         );
     }
-    let ans: u32 = solver
-        .solutions(toggle_vars, false)
-        .map(|v| v.into_iter().map(|int| int.as_u64().unwrap() as u32).sum())
-        .min()
-        .unwrap();
-    ans
+
+    let total = Int::fresh_const("total");
+    solver.assert(&total.eq(Int::add(&toggle_vars)));
+    solver.minimize(&total);
+
+    match solver.check(&[]) {
+        SatResult::Sat => solver
+            .get_model()
+            .unwrap()
+            .eval(&total, true)
+            .and_then(|t| t.as_u64())
+            .unwrap() as u32,
+        _ => panic!("No solution"),
+    }
 }
 
 fn part2(input_data: &InputData) {
